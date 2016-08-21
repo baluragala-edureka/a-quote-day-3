3ebdf87b299e125ddfdb362ab6f3ddff97ad8e84
baluragala <baluragala@gmail.com>
2016-08-21 22:42:36 +0530

    added Procfile and standarized port for heroku

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ app.get('/quote',function(request,response){
   response.sendFile(__dirname+"/quote.html");
 })
 
-app.listen(8000,function(req,res){
+app.listen(8080,function(req,res){
   console.log('Server Started');
 })
